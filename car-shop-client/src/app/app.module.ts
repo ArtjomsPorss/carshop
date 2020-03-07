e7f80ff0169067975b0f6ce85ce29f26784fc2eb
Artyom Porsh <artjom.porsh@gmail.com>
2020-03-07 23:45:39 +0000

    added basic angular routing

--- car-shop-client/src/app/app.module.ts
+++ car-shop-client/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './car/car.component';
+import { NewCarComponent } from './new-car/new-car.component';
+
+const appRoutes: Routes = [
+  { path: 'cars', component: CarsComponent },
+  { path: 'car/:id', component: CarComponent },
+  { path: 'new-car', component: NewCarComponent },
+  { path: '', redirectTo: 'cars', pathMatch: 'full'}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    NewCarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
